96d7d868c46e1122b70c16fbe237c113e9f4f51b
jimmycliff obonyo <cliffjimmy27@gmail.com>
2020-05-23 05:02:37 +0300

    poc send / receive IPC msgs.

--- .desktop/modules/deep-link/index.js
+++ .desktop/modules/deep-link/index.js
@@ -16,50 +16,66 @@ import moduleJson from './module.json';
  * @constructor
  */
 export default class DeepLink {
-    constructor({
-        log, skeletonApp, appSettings, eventsBus, modules, settings, Module
-    }) {
-        const {builderOptions} = appSettings
-        /**
-         * You can delete unused vars from the param destructuring.
-         * Left them here just to emphasize what is passed. Delete the eslint rule at the top
-         * when done.
-         * You can also just have a one `config` param and do `Object.assign(this, config);`
-         */
-        this.module = new Module(moduleJson.name);
+  constructor({
+    log, skeletonApp, appSettings, eventsBus, modules, settings, Module
+  }) {
+    const { builderOptions } = appSettings
+    /**
+     * You can also just have a one `config` param and do `Object.assign(this, config);`
+     */
+    this.module = new Module(moduleJson.name);
 
-        // Get the automatically predefined logger instance.
-        this.log = log;
-        this.eventsBus = eventsBus;
-        this.protocolPrefix = builderOptions.protocols[0].schemes[0]
-        
-        this.eventsBus.on('desktopLoaded', () => {
-            this.init();
-        });
-    }
+    // Get the automatically predefined logger instance.
+    this.log = log;
+    this.eventsBus = eventsBus;
+    this.protocolPrefix = builderOptions.protocols[0].schemes[0]
 
-    init() {
-        // Do some initialization if necessary.
 
-        this.registerApi();
+    this.eventsBus.on('desktopLoaded', () => {
+      this.init();
+      // this.module.send('challegeCode', 'testing');
+      /* this.eventsBus.on('windowCreated', () => {
+        log.info('ok')
+        // win.webContents.send('asynchronous-message', {'SAVED': 'File Saved'});
+        this.module.send('challegeCode', 'testing');
+      }) */
+    });
+  }
 
-        // Lets inform that the module has finished loading.
-        this.eventsBus.emit(`${moduleJson.name}.loaded`);
-    }
+  init() {
+    // Do some initialization if necessary.
 
-    registerApi() {
-        const protocolPrefix = this.protocolPrefix
-        const log = this.log
-        protocol.registerHttpProtocol(protocolPrefix, (req, cb) => {
-            const url = req.url
-            const msg = url.substr(protocolPrefix.length+3)
-            console.log(msg)
-          }, (err) => {
-            if (!err) {
-              log.info(`protocol ${protocolPrefix}:// registered`)
-            } else {
-              log.error(`could not register protocol ${protocolPrefix}://`, err)
-            }
-          })
-    }
+    this.registerApi();
+
+    // Lets inform that the module has finished loading.
+    this.eventsBus.emit(`${moduleJson.name}.loaded`);
+  }
+
+  registerApi() {
+    const protocolPrefix = this.protocolPrefix
+    const log = this.log
+    // const module = this.module
+    // const eventsBus = this.eventsBus
+
+    /* this.module.on('init', (event, fetchId) => {
+      log.info('init received');
+      this.module.respond('init', fetchId, 'blahblah');
+    }); */
+
+    protocol.registerHttpProtocol(protocolPrefix, (req, cb) => {
+      const url = req.url
+      const msg = url.substr(protocolPrefix.length + 3)
+      this.module.call('challegeCode', msg);
+      log(`${msg} Sent`)
+    }, (err) => {
+      if (!err) {
+        log.info(`protocol ${protocolPrefix}:// registered`)
+        this.eventsBus.on('loadingFinished', ()=> {
+          this.module.send('challegeCode', 'init')
+        })
+      } else {
+        log.error(`could not register protocol ${protocolPrefix}://`, err)
+      }
+    })
+  }
 }
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
-export const App = () => (
+export const App = () => {
+
+  if(Meteor.isDesktop){
+    // console.log('desktop')
+    /* const electron = require('electron')
+    electron.ipcRenderer.on('asynchronous-message', (event, arg) => {
+      console.log(arg)
+    }) */
+    /* Desktop.fetch('deep-link', 'init').then((d)=> {
+        console.log(d)
+      }).catch((e)=> {
+        console.log(e)
+      }) */
+    Desktop.once('deep-link', 'challegeCode', (event, arg) => {
+      console.log('challegeCode', arg)
+    })
+  }
+
+  return (
   <div>
     <h1>Welcome to Meteor!</h1>
     <Hello/>
     <Info/>
   </div>
-);
+)};
